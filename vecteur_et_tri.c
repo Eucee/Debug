8c1d4687209b72350d9039e2bff6dd3ee9747e27
Eucée <eucee89@gmail.com>
2016-12-04 17:19:37 +0100

    Debug tri à bulle

--- vecteur_et_tri.c
+++ vecteur_et_tri.c
@@ -3,13 +3,20 @@
 
 /**************************************DEFINITION******************************************/
 
+typedef enum bool bool;
 /*void tri_rapide(int vect[],int taille);
 void triRapide2(int vect[], int debut, int fin);
 int partitionner(int vect[], int debut, int fin);*/
-void echanger(int v[], int i, int j);
-void tri_bulles(int v[], int n);
-void echanger_jusqua_pos(int v[], int dm, int echange);
-void afficherVect(int v[], int taille);
+void echanger(int *adr_vect, int pos1, int pos2);
+void tri_bulles(int *adr_vect, int taille_vect);
+void echanger_jusqua_pos(int *adr_vect, int *adr_taille_a_parcourir, bool aTrier);
+void afficherVect(int *adr_vect, int taille_vect);
+void initialiserVect(int *adr_vect, int taille_vect);
+
+
+enum bool{
+    FALSE, TRUE
+ };
 
 /**************************************FONCTIONS**************************************/
 
@@ -53,68 +60,125 @@ int partitionner(int vect[], int debut, int fin){
     return j;
 }*/
 
-void echanger(int v[], int i, int j){
-    int save = v[i];
-    v[i] = v[j];
-    v[j] = save;
-}
+/*--------------------------------------------------------------------
+PROCEDURE afficherVect:
+-Entrée/Sortie: Pointeur sur vecteur adr_vect, l'adresse du vecteur à modifier
+-Entrée: Entier taille_vect, la taille du vecteur
 
-////////////////////////////////////////////////
+Précondition:
+-Le vecteur ne doit pas être nul
 
-void afficherVect(int v[], int taille){
-    int i;
+Postcondition:
+-Affichage de l'ensemble des éléments d'une vecteurs
+--------------------------------------------------------------------*/
+void afficherVect(int *adr_vect, int taille_vect){
 
-    for (i = 0 ; i < taille ; i++)
-    {
-        printf("%d ", v[i]);
+    //Variable
+    int posVect;//indice de position dans le vecteur
+
+    for (posVect = 0 ; posVect < taille_vect ; posVect++){
+        printf("%d ", adr_vect[posVect]);
     }
     printf("\n");
 }
 
-////////////////////////////////////////////////
+void initialiserVect(int *adr_vect, int taille_vect){
+    //Variable
+    int posVect;//indice de position dans le vecteur
 
-void tri_bulles(int v[], int taille){
+    for (posVect = 0 ; posVect < taille_vect ; posVect++)
+    {
+        adr_vect[posVect] = 0;
+    }
+}
+/*--------------------------------------------------------------------
+PROCEDURE tri_bulles:
+-Entrée/Sortie: Pointeur sur vecteur adr_vect, l'adresse du vecteur à modifier
+-Entrée: Entier pos1, un élément du vecteur à la position x (x!=y)
+-Entrée: Entier pos2, un élément du vecteur à la position y (y!=x)
 
-    //variable globale
-    int dm, echange;
+Précondition:
+-Le vecteur ne doit pas être nul
+
+Postcondition:
+-Echange de position deux valeurs d'un vecteur
+--------------------------------------------------------------------*/
+void echanger(int *adr_vect, int pos1, int pos2){
+    int save; //déclaration d'une variable de sauvegarde
+    save = adr_vect[pos1]; //sauvegarde de l'élément à la pos1
+    adr_vect[pos1] = adr_vect[pos2]; //on écrase la valeur de l'élement à la pos1 par celle à la pos2
+    adr_vect[pos2] = save; //on attribue la valeur sauvegardee (i.e., à l'ancienne pos1) à la pos 2
+}
+
+/*--------------------------------------------------------------------
+PROCEDURE tri_bulles:
+-Entrée/Sortie: Pointeur sur vecteur adr_vect, l'adresse du vecteur à modifier
+-Entrée: Entier taille_vect, la taille du vecteur
+
+Précondition:
+-Le vecteur ne doit pas être nul
+
+Postcondition:
+-Trié par ordre croissant un vecteur contenant des entiers
+--------------------------------------------------------------------*/
+void tri_bulles(int *adr_vect, int taille_vect){
+
+    //Variables
+    int taille_a_parcourir; //déclaration d'un indice temporaire de la longueur du vecteur restant à parcourir
+    bool aTrier; //et d'un booléen pour marquer la fin du tri (= au moins un échange a été réalisé lors du dernier passage)
 
     //affectation
-    dm = taille-1;
-    echange = 1;
+    taille_a_parcourir = taille_vect-1; //le vecteur commence à 0 et finit à taille-1
+    aTrier = TRUE; // initialisation du booléen à FAUX
 
-    while (echange && dm >= 1){
-        echanger_jusqua_pos(v, dm, echange);
+    while (aTrier && taille_a_parcourir >= 1){//tant qu'une derniere modification a été éffectué ET tant qu'il y a plus d'un élément (sinon le tri est trivial), le tableau n'a pas fini son tri
+        echanger_jusqua_pos(adr_vect, &taille_a_parcourir, aTrier); //Cf. fonction ad hoc
         //le maximum est placé correctement, au n-1 passage on a une comparaison de moins a faire
     }
 }
 
-void echanger_jusqua_pos(int v[], int dm, int echange){
+/*--------------------------------------------------------------------
+PROCEDURE echanger_jusqua_pos:
+-Entrée/Sortie: Pointeur sur vecteur adr_vect, l'adresse du vecteur à modifier
+-Entrée/Sortie: Entier taille_vect, la taille du vecteur
+-Entrée: Booléen aTrier, indicatif de fin de tri
 
-    //variable locale
-    int j, k;
+Précondition:
+-Le vecteur ne doit pas être nul
 
-    //affectation
-    j = 0, echange = 0;
+Postcondition:
+-Echange un nombre à une position donnée jusqu'à la fin du vecteur
+--------------------------------------------------------------------*/
+void echanger_jusqua_pos(int *adr_vect, int *adr_taille_a_parcourir, bool aTrier){
 
-    while(j < dm){
-        if(v[j] > v [j+1]){
-            echanger(v, j , j+1);
-            echange = 1 ;
-            k = j;
-            afficherVect(v, 8);
+     //Variables
+    int j, k, posVect; //déclaration
+    j = 0; //affectation de la position temporaire dans le vecteur
+    k = 0;
+    aTrier = FALSE; //valeur par défaut si le programme ne rentre pas dans la condition while
+
+    while(j < *adr_taille_a_parcourir){
+        if(adr_vect[j] > adr_vect[j+1]){//si l'élement suivant du vecteur est supérieur
+            echanger(adr_vect, j , j+1);  //on échange leurs positions (Cf. fonction ad hoc)
+            aTrier = FALSE; //il y a eu une dernière modification effectuée lors de cette itération
+            k = j; //mémorisation de la dernière position d'échange
+            afficherVect(adr_vect, *adr_taille_a_parcourir);
         }
         j++;
     }
-    dm = k;
+    *adr_taille_a_parcourir = k; //actualisation de la longueur du vecteur à parcourir
     //printf("%d\n",j);
 }
 
 int main()
 {
-    int vect[8]= {13,1,23,7,4,14,15,2};
+    int vect[8] = {13,1,23,7,4,14,15,2};
+
+    afficherVect(vect, 8);
+    //initialiserVect(vect, 8);
     //afficherVect(vect, 8);
     tri_bulles(vect, 8);
-    //afficherVect(vect, 8);
+    afficherVect(vect, 8);
 
     return 0;
 }
